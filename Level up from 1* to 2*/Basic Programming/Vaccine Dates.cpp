41493c470ac70bd0a0d762b9e9898fc91b95f6e7
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-20 18:09:21 +0530

    Practice codechef problem

---
+++ Level up from 1* to 2*/Basic Programming/Vaccine Dates.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int d,l,r;
+    cin>>d>>l>>r;
+    
+    if(d >= l && d <= r)
+    {
+        cout<<"Take second dose now";
+    }else if(d > r){
+        cout<<"Too Late";
+    }
+    else if(d < l)
+    {
+        cout<<"Too early";
+    }
+    
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
