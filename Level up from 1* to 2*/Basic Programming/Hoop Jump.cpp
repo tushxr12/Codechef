76d20f5bad076f8e9a7be744bb88e5639b6f3995
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-20 18:33:42 +0530

    Practice codechef problem

---
+++ Level up from 1* to 2*/Basic Programming/Hoop Jump.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;cin>>n;
+    cout<<n/2 + 1;
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
