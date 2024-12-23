1b45bed2e3af2510dfcb5275138c42bdfaf67c82
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-23 14:55:42 +0530

    practice codechef problem

---
+++ Level up from 1* to 2*/Basic Programming/Tom And Jerry 1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a,b,c,d,k;
+    cin>>a>>b>>c>>d>>k;
+    
+    int reqSteps = abs(c - a) + abs(d-b);
+    if(reqSteps > k)
+    {
+        cout<<"NO";
+    }
+    else
+    {
+        if((k - reqSteps)%2 == 0)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+    }
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
