3b0186f89b40dfb7b3d9bcf9dac0040462db6040
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-12 13:23:43 +0530

    practice codechef problem

---
+++ Level up from 1* to 2*/Arrays/A Subtask Problem.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    
+    vector<int> v(n);
+    int zc = 0;
+    for(int i = 0;i < n;i++)
+    {
+        cin>>v[i];
+        if(v[i] == 0)
+            zc++;
+    }
+    
+    if(!zc)
+    {
+        cout<<100;
+        return;
+    }
+    
+    zc = 0;
+    for(int i = 0;i < m;i++)
+    {
+        if(v[i] == 0)
+        {
+            cout<<0;
+            return;
+        }
+    }
+    cout<<k;
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
