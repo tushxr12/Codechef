9d24da483c0818b519d1a423218ef2e9a4f25c1a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-12 17:32:19 +0530

    practice codechef problem

---
+++ Level up from 1* to 2*/Arrays/Nobel Prize.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,m;cin>>n>>m;
+    vector<int> v(n);
+    for(int i = 0;i < n;i++)
+        cin>>v[i];
+    
+    vector<int> temp(m + 1,0);
+    for(int i = 0;i < n;i++)
+    {
+        temp[v[i]]++;
+    }
+    for(int i = 1;i <= m;i++)
+    {
+        if(temp[i] == 0)
+        {
+            cout<<"YES";
+            return;
+        }
+    }
+    cout<<"NO";
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
