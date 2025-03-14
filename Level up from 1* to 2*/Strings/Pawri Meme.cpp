1c5656955c61bedb03efbe02264c4ae946d4167e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-14 17:37:44 +0530

    practice codechef prob

---
+++ Level up from 1* to 2*/Strings/Pawri Meme.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    
+    if(s.size() < 5)
+    {
+        cout<<s;
+        return;
+    }
+    
+    for(int i = 0;i < s.size();i++)
+    {
+        if(s[i] == 'p' && s[i + 1] == 'a' && s[i + 2] == 'r' && s[i + 3] == 't' && s[i + 4] == 'y')
+        {
+            s[i + 2] = 'w';
+            s[i + 3] = 'r';
+            s[i + 4] = 'i';
+            i = i + 4;
+        }
+    }
+    cout<<s;
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
