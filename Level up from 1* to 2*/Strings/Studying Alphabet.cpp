d858f05952204291e65adfbc6946f053d5f9dea3
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-15 13:50:25 +0530

    practice codechef prob

---
+++ Level up from 1* to 2*/Strings/Studying Alphabet.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;
+    cin>>s;
+    set<char> st;
+    for(auto i : s)
+        st.insert(i);
+    int n;cin>>n;
+    bool flag = false;
+    while(n--)
+    {
+        flag = false;
+        string s;cin>>s;
+        for(auto i : s)
+            if(st.find(i) == st.end())
+                {
+                    cout<<"NO\n";
+                    flag = true;
+                    break;
+                }
+        if(flag)
+            continue;
+        else
+            cout<<"YES\n";
+    }
+    return 0;
+}
