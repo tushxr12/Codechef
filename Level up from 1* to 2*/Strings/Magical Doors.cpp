11bf096871edcfb1c4672c0e7ba100534b98f011
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-17 22:46:33 +0530

    practice daily prob

---
+++ Level up from 1* to 2*/Strings/Magical Doors.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    
+    int ans = 0;
+    if(s[0] == '0')
+        ans = 1;
+    
+    for(int i = 1;i < s.size();i++)
+        if(s[i] != s[i - 1])
+        {
+            ans++;
+        }
+    cout<<ans;
+}
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
