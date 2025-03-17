f06b19269b9fed70f5546e9ff98998ddccf20770
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-17 16:00:11 +0530

    practice codechef prob

---
+++ Level up from 1* to 2*/Strings/Lapindromes.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    
+    int n = s.size();
+    map<char,int> left, right;
+    if(n%2)
+    {
+        for(int i = 0;i < n/2;i++)
+        {
+            left[s[i]]++;
+        }
+        
+        for(int i = n/2 + 1;i < n;i++)
+        {
+            right[s[i]]++;
+        }
+    }   
+    else
+    {
+        for(int i = 0;i < n/2;i++)
+        {
+            left[s[i]]++;
+        }
+        
+        for(int i = n/2;i < n;i++)
+        {
+            right[s[i]]++;
+        }
+    }
+    // cout<<"Left map : \n";
+    // for(auto i : left)
+    // {
+    //     cout<<i.first<<" "<<i.second<<"\n";
+    // }
+    // cout<<"\n";
+    // cout<<"Right map : \n";
+    // for(auto i : right)
+    // {
+    //     cout<<i.first<<" "<<i.second<<"\n";
+    // }
+    // cout<<"\n";
+    for(auto i : left)
+    {
+        if(right.find(i.first) == right.end() || i.second != right[i.first])
+        {
+            cout<<"NO";
+            return;
+        }
+    }
+    cout<<"YES";
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
