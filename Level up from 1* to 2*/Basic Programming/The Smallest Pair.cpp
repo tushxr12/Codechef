405f1fff44789e61de5843a46f9a98daac333fef
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-18 13:04:59 +0530

    practice codechef prob

---
+++ Level up from 1* to 2*/Basic Programming/The Smallest Pair.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i = 0;i < n;i++)
+        cin>>v[i];
+    
+    sort(v.begin(), v.end());
+    cout<<v[0] + v[1];
+}
+
+int main() {
+	// your code goes here
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+}
