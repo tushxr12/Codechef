ddd2cd5e83c586d540486af5dc070acc95f370c9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-18 13:27:43 +0530

    practice codechef prob

--- Level up from 1* to 2*/Sorting/Maximum Weight Difference.cpp
+++ Level up from 1* to 2*/Sorting/Maximum Weight Difference.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void solve()
 {
-    int n,k;
+    long long n,k;
     cin>>n>>k;
-    int totalSum = 0;
+    long long totalSum = 0;
     vector<int> v(n);
     for(int i = 0;i < n;i++)
     {
@@ -15,12 +15,31 @@ void solve()
         
         
     sort(v.begin(), v.end());
-    int sonSum = 0, fatherSum = 0;
+    long long sonSum = 0, fatherSum = 0;
     for(int i = 0;i < k;i++)
         sonSum += v[i];
-    
     fatherSum = totalSum - sonSum;
-    cout<<fatherSum - sonSum;
+    
+    long long sonSum2 = 0, fatherSum2 = 0;
+    sort(v.begin() , v.end(), greater<int>());
+    
+    for(int i = 0;i < k;i++)
+        sonSum2 += v[i];
+    
+    fatherSum2 = totalSum - sonSum2;
+    // cout<<"Total sum : "<<totalSum<<endl;
+    // cout<<"Son sum : "<<sonSum<<endl;
+    // cout<<"Father Sum : "<<fatherSum<<endl;
+    
+    // cout<<"Total sum : "<<totalSum<<endl;
+    // cout<<"Son sum 2: "<<sonSum2<<endl;
+    // cout<<"Father Sum 2: "<<fatherSum2<<endl;
+    
+    long long res1 = fatherSum - sonSum;
+    long long res2 = abs(sonSum2 - fatherSum2);
+    
+    // cout<<"Res1 and Res2 : "<<res1<<" "<<res2<<endl;
+    cout<<max(res1, res2);
 }
 
 int main() {
