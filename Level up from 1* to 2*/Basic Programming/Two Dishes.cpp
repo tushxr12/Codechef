b6bb24dbc4e69a782c8d1d6b802357d2410f3569
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-03-20 10:24:41 +0530

    practice codechef prob

--- Level up from 1* to 2*/Basic Programming/Two Dishes.cpp
+++ Level up from 1* to 2*/Basic Programming/Two Dishes.cpp
@@ -4,13 +4,7 @@ using namespace std;
 void solve()
 {
     int n,a,b,c;
-    cin>>n>>a>>b>>c; // a fruit, b veg, c fish
-    
-    // 1fruit & 1veg
-    // 1veg & 1fish
-    
-    //3 2 2 2 
-    //5 5 3 2
+    cin>>n>>a>>b>>c; 
     int firstDishCount = min(a,b);
     int bRem = b - firstDishCount;
     int secondDishCount = min(bRem,c);
@@ -26,7 +20,6 @@ void solve()
 }
 
 int main() {
-	// your code goes here
     int t;cin>>t;
     while(t--)
     {
