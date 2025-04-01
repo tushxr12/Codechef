216128e69f4c6245747a4ddb543476fe969195cf
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-01 18:58:22 +0530

    practice codechef prob

---
+++ Practice Java/Area of circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        System.out.println(3.14*(r*r));
+	}
+}
