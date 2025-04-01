e85fd0541ef855e92590e83102c52908797fe348
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-01 18:57:55 +0530

    practice codechef prob

---
+++ Practice Java/Debug this code.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        System.out.println(one / two);
+    }
+}
