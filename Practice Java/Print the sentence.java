31fc7e9fb276d2300a16f8681fd6c0c76c4703ee
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-01 18:59:08 +0530

    practice codechef prob

---
+++ Practice Java/Print the sentence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		System.out.println(s1 + " " + s2);
+	}
+}    
