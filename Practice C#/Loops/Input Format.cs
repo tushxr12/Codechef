1b35a2ac6d58e5db31173aae96bdaa5f21b88a33
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-05 03:46:49 +0530

    practice C# prob

--- Practice C#/Loops/Input Format.cs
+++ Practice C#/Loops/Input Format.cs
@@ -13,7 +13,6 @@ class Program
             // Parse the input values into integers
             int a = int.Parse(input[0]);
             int b = int.Parse(input[1]);
-            // Write your code here
             Console.WriteLine(a + b);
         }
     }
