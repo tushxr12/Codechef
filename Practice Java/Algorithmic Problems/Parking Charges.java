cf875df4698d6c131d8b31695d88c7c8803854b6
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-06 20:54:25 +0530

    practice java prob

---
+++ Practice Java/Algorithmic Problems/Parking Charges.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int h = sc.nextInt();
+        
+        System.out.println(x + y*(h - 1));
+	}
+}
