c4a682c713e1bcbf3bfb28a4f4b3daa7d588eebd
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-06 20:53:54 +0530

    practice java prob

---
+++ Practice Java/Algorithmic Problems/Learning SQL.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int e = sc.nextInt();
+        
+        System.out.println((r + e)*c);
+	}
+}
