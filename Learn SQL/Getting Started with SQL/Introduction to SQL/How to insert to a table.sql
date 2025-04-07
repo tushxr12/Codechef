d0b26c9ef49c62f513f905af5b3c09295536ec95
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-08 00:54:54 +0530

    practice sql prob

---
+++ Learn SQL/Getting Started with SQL/Introduction to SQL/How to insert to a table.sql
@@ -0,0 +1,4 @@
+/* Write a query to add the below mentioned employee details to the EMPLOYEE table.
+Employee id: 06, Employee Name: Brandon Kim, Department: Operations
+*/
+INSERT into Employee (Employee_id, Employee_name, Department) VALUES (6, 'Brandon Kim' , 'Operations');
