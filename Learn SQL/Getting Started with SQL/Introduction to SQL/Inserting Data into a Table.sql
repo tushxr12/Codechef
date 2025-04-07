4d2c72aec743891c0558f445bfe0679c6bb32d7b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-08 00:52:43 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Introduction to SQL/Inserting Data into a Table.sql
@@ -0,0 +1,4 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+INSERT into employee (employee_id, employee_name, department) VALUES (4, 'Marcus Garcia', 'Product');
+INSERT into employee (employee_id, employee_name, department) VALUES (5, 'Samantha Park', 'Hr');
