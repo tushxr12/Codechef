34ca3e681d7a2df97b59d527e2497693ebbb125e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-08 00:56:16 +0530

    practice sql prob

---
+++ Learn SQL/Getting Started with SQL/Introduction to SQL/Debug this query.sql
@@ -0,0 +1,8 @@
+/* The Query written in the console is trying to insert data to the table employee.
+Debug this query to output the entire table */
+
+INSERT INTO employee (Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway');
+
+SELECT * FROM employee; 
