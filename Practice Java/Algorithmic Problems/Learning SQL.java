f48034763cc0dbf340bb9fea234a0e828ef43dd9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-07 23:35:46 +0530

    practice java prob

--- Practice Java/Algorithmic Problems/Learning SQL.java
+++ Practice Java/Algorithmic Problems/Learning SQL.java
@@ -6,7 +6,6 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
         int c = sc.nextInt();
