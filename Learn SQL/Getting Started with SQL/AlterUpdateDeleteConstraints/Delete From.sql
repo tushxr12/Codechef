abec6c111f7764fabd74bb7ed5ec2b70d4aac348
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-09 10:25:43 +0530

    practice SQL Prob

---
+++ Learn SQL/Getting Started with SQL/AlterUpdateDeleteConstraints/Delete From.sql
@@ -0,0 +1,6 @@
+/* Write a query which does the following
+- Delete all rows in the employee table whose Department is 'Hr'.
+- Output all the entires of the table. */
+
+Delete from employee where Department='Hr';
+select * FROM employee;
