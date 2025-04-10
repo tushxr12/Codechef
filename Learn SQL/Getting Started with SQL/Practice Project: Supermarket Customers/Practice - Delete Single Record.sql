4ed0cf033d711c0c1b20aa0575230d746d99ae5f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-10 09:32:27 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Practice Project: Supermarket Customers/Practice - Delete Single Record.sql
@@ -0,0 +1,6 @@
+/*Write a query which does the following
+- Delete the entire row of the customer with customer id 1.
+- Output the entire table once the above action is performed. */
+
+Delete from customer where Id = 1;
+select * FROM customer;
