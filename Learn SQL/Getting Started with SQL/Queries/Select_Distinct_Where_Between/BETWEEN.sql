de744b96fdf519e153ffba3c704ab5da054be6f9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-10 16:53:48 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Queries/Select_Distinct_Where_Between/BETWEEN.sql
@@ -0,0 +1,5 @@
+/* Write a query to do the following
+- Output all entries from the table
+- Where passenger_name between 'A' and 'D' */
+
+select * FROM flights where passenger_name between 'A' And 'D';
