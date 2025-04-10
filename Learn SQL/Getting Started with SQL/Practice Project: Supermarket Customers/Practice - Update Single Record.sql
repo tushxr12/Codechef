2499ccad2588ab2913be85e7db08ebf11c2c2e5e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-10 09:31:47 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Practice Project: Supermarket Customers/Practice - Update Single Record.sql
@@ -0,0 +1,6 @@
+/* Write a query which does the following
+- Update the age to 52 for the customer with customer id 3.
+- Output all the entires of the table. */
+
+Update customer set Age = 52 where Id = 3;
+select * from customer;
