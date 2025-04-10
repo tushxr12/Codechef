c8f40d8352bbc4c6453e93f6ffeb1bb78a1f6a2e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-10 16:54:34 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Queries/Select_Distinct_Where_Between/Practice - Basic query with Where Clause.sql
@@ -0,0 +1,5 @@
+/* Write a query that does the following
+- Where the origin of the flight is 'New York'
+- Output the passenger_name and gender */
+
+select passenger_name,gender from flights where origin = 'New York'; 
