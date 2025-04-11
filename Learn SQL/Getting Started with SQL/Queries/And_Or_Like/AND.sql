09c770853d9821f4137e1cf4dd69b21a275c3697
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-11 13:13:59 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Queries/And_Or_Like/AND.sql
@@ -0,0 +1,5 @@
+/* write a query below that returns the rows which meet the following conditions
+- Gender - Female
+- Destination - Cairo */
+
+select * from flights where Gender = 'Female' AND Destination = 'Cairo';
