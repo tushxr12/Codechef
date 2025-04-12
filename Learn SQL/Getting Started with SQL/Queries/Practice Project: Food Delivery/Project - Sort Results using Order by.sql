13aa172effcb7e40fa33042f56ba6d7e198a9f60
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-12 12:55:34 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Queries/Practice Project: Food Delivery/Project - Sort Results using Order by.sql
@@ -0,0 +1,4 @@
+/* You want to ensure that your delivery boys have to climb the least floors.
+Write a query to sort the data in an ascending order. */
+
+select * from orders ORDER BY apartment_floor;
