175b50f9374caefb4eb9cea9ff34309602c0fdec
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-13 18:26:25 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Functions/Date and Time Functions/Manipulating date and time.sql
@@ -0,0 +1,5 @@
+/* Write a query which outputs the date of 1 month from '2024-07-04'.
+   Write a query which outputs the time of 30 minutes from '20:34:21'.*/
+
+select DATE('2024-07-04', '+1 month');
+select TIME('20:34:21', '+30 minutes');
