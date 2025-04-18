6cb7c7b393822ede08e10ba0daf9ca4eda667097
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-18 12:28:02 +0530

    practice SQL prob

--- Learn SQL/Getting Started with SQL/Functions/Count_Max_Min_Round/MAX() and MIN().sql
+++ Learn SQL/Getting Started with SQL/Functions/Count_Max_Min_Round/MAX() and MIN().sql
@@ -1,4 +1,4 @@
 /*Write a query to find the highest and lowest 'Hourly_pay' of the employees from the table 'employee'*/
 
-select MAX(Hourly_pay) as max_pay from employee;
-select MIN(Hourly_pay) as min_pay from employee;
+select MAX(Hourly_pay) as max_pay from employee; /*Max*/
+select MIN(Hourly_pay) as min_pay from employee; /*Min*/
