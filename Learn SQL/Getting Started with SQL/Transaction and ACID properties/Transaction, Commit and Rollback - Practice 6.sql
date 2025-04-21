66b2bf8354d91baa8dc877e56f2c91b536055bad
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-21 09:35:48 +0530

    practice SQL prob

---
+++ Learn SQL/Getting Started with SQL/Transaction and ACID properties/Transaction, Commit and Rollback - Practice 6.sql
@@ -0,0 +1,19 @@
+-- Fill in the missing command to start the transaction
+BEGIN TRANSACTION;
+
+-- Insert the first booking
+INSERT INTO Bookings (CustomerID, RoomNumber, CheckInDate, CheckOutDate) 
+VALUES (201, 101, '2025-06-01', '2025-06-05');
+
+-- Fill in the missing command to create a savepoint
+SAVEPOINT first_booking;
+
+-- Insert the second booking
+INSERT INTO Bookings (CustomerID, RoomNumber, CheckInDate, CheckOutDate) 
+VALUES (201, 102, '2025-06-01', '2025-06-05');
+
+-- If the customer decides to cancel the second booking, remove only that entry
+ROLLBACK TO first_booking;
+
+-- Finalize the transaction
+COMMIT;
