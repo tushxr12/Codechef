461a555410341ece98468a7ab660e34f6d57b3f8
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-04-27 18:06:35 +0530

    practice SQL prob

---
+++ Learn SQL/Practice Projects using SQL/School Database Management System/Task-4E Data Modification (Update and Alter).sql
@@ -0,0 +1,6 @@
+/* Update your query here */
+Update Courses
+SET department = 'Technology'
+WHERE course_name = 'Computer Science';
+
+select course_id, course_name, department FROM Courses where department = 'Technology';
