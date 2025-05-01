4d792852d67688a6de8737d727e3540c5afb3289
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-01 22:47:35 +0530

    practice python prob

---
+++ Learn Python Programming/Debug your code/Practice Debugging/Debug the code - 6.py
@@ -0,0 +1,9 @@
+number = int(input())
+if number > 0:
+    print("The number is positive")
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+else:
+    print("The number is negative")
