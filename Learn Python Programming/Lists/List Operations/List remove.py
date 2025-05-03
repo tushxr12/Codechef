4b3fbfbc079d01bcb16a7d9ef6df3343a5f64455
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-03 14:10:48 +0530

    practice python prob

---
+++ Learn Python Programming/Lists/List Operations/List remove.py
@@ -0,0 +1,6 @@
+# Update the code below to solve the problem
+
+random =  ['beetroot', 'horlicks', 'pineapple', 'peanuts', 'beetroot', 'horlicks', 'peanuts', 'apple']
+ok = input()
+random.remove(ok)
+print(random)
