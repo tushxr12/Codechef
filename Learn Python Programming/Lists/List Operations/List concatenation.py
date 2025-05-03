b08c6bfde9e36effa80273ea53ae12853534b547
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-03 14:14:03 +0530

    practice python prob

---
+++ Learn Python Programming/Lists/List Operations/List concatenation.py
@@ -0,0 +1,7 @@
+# Update the code below to solve the problem
+
+list1 = ['Annihilate', 'Die for you', 'Am I dreaming', 'Burn the house down']
+list2 = ['Hero', 'Maybe Man', 'Run', 'Enemy']
+
+joined_list = list2 + list1
+print(joined_list)
