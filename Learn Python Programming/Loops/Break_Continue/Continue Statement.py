1609046bfc4a0d690da4156da759f60acffc3ae9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-06 19:27:53 +0530

    practice python prob

---
+++ Learn Python Programming/Loops/Break_Continue/Continue Statement.py
@@ -0,0 +1,9 @@
+# update the code below to solve the problem
+
+n = int(input())
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    if number == n:
+        continue
+    print(number)
