3c62473864fb83b06e9f293c6ac6c8eb1a9ab406
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-07 14:45:37 +0530

    practice python prob

---
+++ Learn Python Programming/Use functions in problems/Coding problem-2.py
@@ -0,0 +1,15 @@
+# Update the code below to solve the problem
+
+def square(t):
+    return t*t;
+
+    
+def half(i):
+    return i//2;
+ 
+ 
+ 
+n = int(input())
+result1 = square(n)
+result2 = half(result1)
+print(result2)
