185498ec99e914ef0f9f50f2e9218af3db47810f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 15:40:38 +0530

    practice python prob

---
+++ Learn Python Programming/Tuples an Dictionary/Tuple unpacking.py
@@ -0,0 +1,8 @@
+student = (["Alice", "Bob"], ["Math", "Science", "English"], 20, 'XYZ')
+
+# Update the code below this line
+first, second, third, fourth = student
+print(first[1])
+print(second[2])
+print(third)
+print(fourth)
