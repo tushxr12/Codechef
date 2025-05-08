149102d198aee8f90f967c371b8ca4479fad7ced
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 15:52:25 +0530

    practice python prob

---
+++ Learn Python Programming/Tuples an Dictionary/More Abput Dictionary/Nested dictionary.py
@@ -0,0 +1,13 @@
+# Update the code below
+
+nested_dict = {
+    "person1": {"name": "Alice", "age": 30},
+    "person2": {"name": "Bob", "age": 25}
+}
+
+
+nested_dict["person1"]["age"] = 35
+nested_dict["person2"]["location"] = "New York"
+nested_dict["person3"] = {"name":"Charlie", "age":28}
+
+print(nested_dict)
