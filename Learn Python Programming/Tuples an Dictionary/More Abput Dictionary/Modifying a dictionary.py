0f33333f875c3a17ec01164e2e7535d550b80b9c
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 15:46:11 +0530

    practice python prob

---
+++ Learn Python Programming/Tuples an Dictionary/More Abput Dictionary/Modifying a dictionary.py
@@ -0,0 +1,13 @@
+# Update the code below to get the necessart output
+
+student_info = {"name": "Alice", "age": 25, "course": "Python"}
+
+# Adding a new key-value pair
+student_info["location"] = "New York"
+
+
+# Modifying an existing value
+student_info["age"] = 26
+
+
+print("Updated Student Info:", student_info)
