f2911c1b6393de06531d1192d3fbd45207656ae7
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 15:47:50 +0530

    practice python prob

---
+++ Learn Python Programming/Tuples an Dictionary/More Abput Dictionary/Iterate over a dictionary.py
@@ -0,0 +1,6 @@
+# Debug the code below to solve the problem
+
+student_info = {"name": "Alice", "age": 25, "course": "Python"}
+
+for (info,value) in student_info.items():
+    print("Key:", info, ", Value:", value)
