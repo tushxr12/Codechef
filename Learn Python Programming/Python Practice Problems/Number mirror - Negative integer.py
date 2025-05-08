3fe974803c557d0b0b61410094c43272ecd3dbe4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 18:58:37 +0530

    practice python prob

---
+++ Learn Python Programming/Python Practice Problems/Number mirror - Negative integer.py
@@ -0,0 +1,9 @@
+# accept the count of test cases given in the the 1st line
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    n *= -1
+    print(n)
+# run a for loop for each test case
+# then for each test case input an integer and output it's negative
