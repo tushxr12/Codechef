7142c7d7a868fedec5821b22fb5a01bfa2eee5bb
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-08 15:48:27 +0530

    practice python prob

---
+++ Learn Python Programming/Tuples an Dictionary/More Abput Dictionary/Key existence in a dictionary.py
@@ -0,0 +1,11 @@
+# Click on submit to view the result
+
+student_info = {"name": "Alice", "age": 25, "course": "Python"}
+
+# Using the 'in' operator to check for key existence
+print("name" in student_info)  
+print("location" in student_info)  
+
+# Using the 'get()' method to check for key existence
+print(student_info.get("age"))  
+print(student_info.get("location"))  
