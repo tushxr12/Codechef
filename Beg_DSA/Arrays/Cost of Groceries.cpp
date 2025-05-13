19da30d81b10608605bc7ef9b8bbe608b3b845e9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-13 13:57:33 +0530

    practice array prob

---
+++ Beg_DSA/Arrays/Cost of Groceries.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        int a[n],b[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int j=0;j<n;j++){
+            cin>>b[j];
+        }
+        // your code goes here
+        int cost = 0;
+        for(int i = 0;i < n;i++)
+        {
+            if(a[i] >= x)
+                cost += b[i];
+        }
+        cout<<cost<<"\n";
+    }
+}
