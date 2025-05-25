65a16d934f89793669f41294904142ec75562b6a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-25 18:51:43 +0530

    learn GO

---
+++ Learn GO/Variables and Data Types/More Data types/Negative Numbers.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	a := -50
+	b := 40
+	// Complete your code from here
+	sum := a + b;
+	product := a*b
+	quotient := a/b
+	fmt.Println(sum)
+	fmt.Println(product)
+	fmt.Println(quotient)
+}
