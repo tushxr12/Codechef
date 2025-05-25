80adb1a9d8373a16bbb688024e89587a8618eade
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-25 18:12:07 +0530

    learn GO

--- Learn GO/Variables and Data Types/Intro to Variables and Data Types/Double datatype.go
+++ Learn GO/Variables and Data Types/Intro to Variables and Data Types/Double datatype.go
@@ -2,8 +2,8 @@ package main
 import "fmt"
 
 func main() {
-	pi float32 := 3.14
-	var radius float32 =  3.14 // radius has to be declared as a 'float32'
+	var pi float32 = 3.14;
+	var radius float32 =  8.9; // radius has to be declared as a 'float32'
 	area := pi * radius * radius
 	fmt.Print("The Area of the given Circle is ")
 	fmt.Print(area)
