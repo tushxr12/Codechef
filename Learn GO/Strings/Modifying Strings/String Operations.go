be78d052efad9eae7dfbf4dc07d7b600686fb00b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-27 09:19:29 +0530

    learn GO

---
+++ Learn GO/Strings/Modifying Strings/String Operations.go
@@ -0,0 +1,12 @@
+// Update the blanks below to print the first 3 characters of the String
+// There is also a syntax error - try and fix it to debug the code
+
+package main
+import "fmt"
+
+func main() {
+	vari := "String"
+	fmt.Print(string(vari[0])) 
+	fmt.Print(string(vari[1]))
+	fmt.Print(string(vari[2]))
+}
