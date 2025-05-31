ed4b55bd7401270109c9cff794d4d6a39c2d5731
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-31 12:21:58 +0530

    learn GO

---
+++ Learn GO/Conditional Statements/If_else/If & Else Statements.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+	Age := 25
+	Vage := 18
+
+	if Age > Vage {
+		fmt.Println("Old enough to vote!")
+	} else {
+		fmt.Println("Not old enough to vote.")
+	}
+}
