84cc7279304415327c51c72ac3fac66e5e546e87
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-31 12:20:13 +0530

    learn GO

---
+++ Learn GO/User Inputs/Practice Problems/Table of Any Number.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+	fmt.Scan(&num)
+
+	fmt.Println(num, "x 1 =", num*1)
+	fmt.Println(num, "x 2 =", num*2)
+	fmt.Println(num, "x 3 =", num*3)
+	fmt.Println(num, "x 4 =", num*4)
+	fmt.Println(num, "x 5 =", num*5)
+	fmt.Println(num, "x 6 =", num*6)
+	fmt.Println(num, "x 7 =", num*7)
+	fmt.Println(num, "x 8 =", num*8)
+	fmt.Println(num, "x 9 =", num*9)
+	fmt.Println(num, "x 10 =", num*10)
+}
